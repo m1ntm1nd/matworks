03a43255f20dc58689057379fff8414a26ff0537
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2022-03-16 22:56:16 +0300

    Update script2.m

--- script2.m
+++ script2.m
@@ -41,14 +41,7 @@ function  mas = addTowersRec(xplt, yplt, numTiers, isd, rad)
             offsetY = yplt(c)
             %mas(end+1) = offsetX
             xplt = horzcat(xplt, x-offsetX)
-            yplt =  horzcat(yplt, y-offsetY)
-            
-            pgon1 = nsidedpoly(6, 'Center', [offsetX + rad/2, offsetY + sqrt(3)/2 * rad], 'Sidelength', rad)
-            pgon2 = nsidedpoly(6, 'Center', [offsetX + rad/2, offsetY - sqrt(3)/2 * rad], 'Sidelength', rad)
-            pgon3 = nsidedpoly(6, 'Center', [offsetX - rad, offsetY], 'Sidelength', rad)
-            
-            plot([pgon1, pgon2, pgon3])
-            
+            yplt =  horzcat(yplt, y-offsetY)        
             
             addTowersRec(xplt, yplt, numTiers-1, isd, rad)
         end
