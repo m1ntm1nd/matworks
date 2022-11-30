4996863e6058e43b6ae759df17cda019c042e405
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2022-12-01 01:42:33 +0300

    Create matched.m

---
+++ course/matched.m
@@ -0,0 +1,49 @@
+function fallibility;
+clear all;
+%a = [0 0 0 0 +1 +1 +1 -1 0 0 0 0 +1 +1 +1 -1 0 0 0 0]'; % код Баркера
+a = [+1 +1 +1 -1 0 0 0 0 0 0 0 0]';
+N=length(a); % кол-во временных выборок
+M=4; % кол-во элементов решетки
+t = (0:1:N-1);
+
+snr0 = 0;
+SKO = 1;
+SKO_ = SKO;
+snr_=snr0;
+for snr = -5:5
+    snr_dB = 10^(snr/10);% перевод из дБ
+    A = sqrt(snr_dB);
+    teta = pi/3; % угол прихода сигнала по отношению к нормали к апертуре АР
+    fi0 = 0; % начальная фаза
+    delta_fi = 2*pi*sin(teta)*0.5; %0.5 = d/lambda, где lambda - длина волны, d - расстояние между элементами,
+    m = 1;
+
+    for m = m:(M-1)
+        Signal1(m) = exp(1i*(fi0+m*delta_fi));
+    end
+
+    Signal = [exp(1i*fi0) Signal1];
+    S = A*a*Signal;
+    
+    Acc = zeros(size(S));
+    for number_tests = 1:10000 % количество испытаний
+        noise = (randn(N, M)+1i*randn(N, M))/sqrt(2); % добавляет белый гауссов шум к векторному сигналу
+        x = S + noise; % складываем сигнал и шум
+        y = filter(S(4:-1:1),1,x); % на выходе СФ
+        Acc = (Acc+y);
+    end
+    Acc = Acc / 10000;
+
+    %plot corr func
+    %for i = 1:M
+        %plot(t, abs(Acc(1:end, i)), 'r|-');
+        %hold on;
+    %end
+    plot(t, abs(Acc(1:end, 1)), 'r|-');
+
+
+    title("Correlation");
+end
+
+
+end
